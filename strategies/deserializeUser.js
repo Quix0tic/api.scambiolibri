014030b203d9f34e9d1014538f1195a049d89f1a
Marco <bortolanmarco@gmail.com>
2016-12-30 17:49:51 +0100

    serialize & deserialize

--- strategies/deserializeUser.js
+++ strategies/deserializeUser.js
@@ -1,7 +1,7 @@
 // Executed at every request to insert user object inside req variable
 module.exports = function(User) {
-    var deserialize = function(id, done) {
-        User.findById(id).then(function(foundUser) {
+    var deserialize = function(phone, done) {
+        User.findByPrimary(phone).then(function(foundUser) {
             return done(null, foundUser);
         }, function(err) {
             return done(err);
