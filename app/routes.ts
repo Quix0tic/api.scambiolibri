bde20aede098020bcc3dcc4ec84d754d271845b2
Marco <bortolanmarco@gmail.com>
2017-05-06 21:44:34 +0200

    Invalid char

--- app/routes.ts
+++ app/routes.ts
@@ -68,7 +68,7 @@ var fcm = new FCM(process.env.FCM_KEY || '');
 function notification(city: String, isbn: string, title: string) {
 
     fcm.send({
-        to: city.concat("/", isbn),
+        to: city.concat("_", isbn),
         notification: {
             title: 'Libri disponibili',
             body: 'È disponibile il libro "' + title + '"'
